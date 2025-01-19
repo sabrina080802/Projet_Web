4db5dfd2a9a19460fa3aa2be9ef1e32769197bba
Sabrina AL HAMMUTI <sabrina080802@hotmail.fr>
2025-01-19 12:23:33 +0100

    modification of the bdd

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     ACCESS_TOKEN_SECRET: 'EMMA123',
     DB: {
         host: 'mysql-salhammuti.alwaysdata.net',
-        user: 'salhammuti',
+        user: '395015',
         password: 'sabrina080802*',
         database: 'salhammuti_sephory'
     }
